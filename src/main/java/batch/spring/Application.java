3dff5b580b7b8d4cff69a830c32cd8bf6445671b
HongJungWan <hjw43ok@hs.ac.kr>
2023-05-14 13:04:08 +0900

    feat: 기본 환경 설정 ver.0.0.2

--- src/main/java/batch/spring/Application.java
+++ src/main/java/batch/spring/Application.java
@@ -1,13 +1,13 @@
 package batch.spring;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }

a92651f4aac53423f4fee4292d62c91a15363847
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2023-05-15 08:57:55 +0900

    Feature/4 db migration (#4)
    * feat: Accounts 도메인 구현
    
    * feat: Orders 도메인 구현
    
    * docs: sql_script.sql
    
    * feat: 주문 테이블 -> 정산 테이블 <데이터 이관> 배치 구현

---
+++ src/main/java/batch/spring/core/domain/orders/Orders.java
@@ -0,0 +1,35 @@
+package batch.spring.core.domain.orders;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String orderItem;
+    private Integer price;
+    private Date orderDate;
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "id=" + id +
+                ", orderItem='" + orderItem + '\'' +
+                ", price=" + price +
+                ", orderDate=" + orderDate +
+                '}';
+    }
+}
